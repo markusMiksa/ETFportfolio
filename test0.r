7e27d5fb238dcbe96d3acbd99f90a6134d7c2edf
mmiksa <markus.miksa@web.de>
2017-02-02 15:08:49 +0100

    i

--- test0.r
+++ test0.r
@@ -8,3 +8,7 @@ e <- "2.än derung in new-branch"
 f <- "3.änderung in new-branch"
 g <-"4.änderung"    
 h <-"5.ände"  
+
+#h wurde in den master gmerged  -- jetzts gehts lustig weiter
+#mit neuen features ...
+
